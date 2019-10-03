29dce12e0d4834060fe53c282d375c65b33d695a
pcurich <curichpedro@gmail.com>
2019-10-03 14:44:30 -0500

    first commit

---
+++ social-icon.js
@@ -0,0 +1,15 @@
+import { LitElement, html } from 'lit-element';
+
+export class SocialIcon extends LitElement {
+
+    static styles = css`
+    :host {
+        display: block;
+    }
+    `;
+
+    render() {
+        return html``;
+    }
+}
+customElements.define('social-icon', SocialIcon);
